02d842940697916107b0cdef42aa010cf7f08bc4
Hohnn <jean-bateee@hotmail.fr>
2022-08-30 17:16:27 +0200

    rework drone page & addimage

--- src/components/drone/DronePage.js
+++ src/components/drone/DronePage.js
@@ -19,55 +19,75 @@ export default function DronePage() {
     let imgArray = []
     const { user } = useContext(UserContext)
     const { id } = useParams()
-    const [drone, setDrone] = useState([])
-    const [images, setImages] = useState([])
+   /*  const [drone, setDrone] = useState([]) */
+    /* const [images, setImages] = useState([]) */
     const [load, setLoad] = useState(true)
     const [image, setImage] = useState([])
-
-    useEffect (() => {
-        fetch('https://skydrone-api.herokuapp.com/api/v1/drones/' + id)
-            .then(res => res.json())
-            .then(data => {
-                setDrone(data)
-            })
-    }, [])
-    useEffect (() => {
-        fetch('https://skydrone-api.herokuapp.com/api/v1/images/' + id)
-        .then(response => response.json())
-        .then(data => {
-            setImages([])
-            data.forEach(element => {
-                let url = `data:image/png;base64,${toBase64(element.img.data)}`
-                setImages(previousState => [...previousState, url])
-                setLoad(false)
-                
-            });
-            
-            })
-        }
-    , [])
-
-    images.forEach(element => {
-        imgArray.push(<img src={element} alt="presentation" />)
+    const [data, setData] = useState({
+        drone: {},
+        images: []
     })
+    useEffect (() => {
+        let drone, 
+            images
+        async function fetchData() {
+            if (id) {
+                await fetch('https://skydrone-api.herokuapp.com/api/v1/drones/' + id)
+                    .then(res => res.json())
+                    .then(data => {
+                        drone = data
+                    })
+
+                await fetch('https://skydrone-api.herokuapp.com/api/v1/images/' + id)
+                    .then(response => response.json())
+                    .then(data => {
+                        images = []
+                        data.forEach(element => {
+                            let url = `data:image/png;base64,${toBase64(element.img.data)}`
+                            images.push(url) 
+                            setLoad(false)
+                        });
+                    
+                    })
+            }
+        }
+
+        async function setFetchData() {
+            await fetchData()
+            setData({
+                drone: drone,
+                images: images
+            })
+            data.images.forEach(element => {
+                imgArray.push(<img src={element} alt="presentation" />)
+            })
+            console.log(data.images);
+        }
+
+        setFetchData()
+
+    }, [])
+
+
+    
+
+    const handleSubmitNew = (event) => {
+        console.log('new drone');
+    }
 
     const handleSubmit = (event) => {
-        console.log(`
+    /*     console.log(`
             title: ${title}
             desc: ${desc}
             price: ${price}
-        `);
+        `); */
 
         fetch('https://skydrone-api.herokuapp.com/api/v1/drones/' + id, {
             method: 'PATCH',
             headers: {
                 'Content-Type': 'application/json',
                 'Authorization': 'Bearer ' + user.token},
-            body: JSON.stringify({
-                name_d: title,
-                description_d: desc,
-                pricePerDay_d: price,
-                })
+            body: JSON.stringify(data.drone)
             })
             .then(res => res.json())
             .then(data => {
@@ -75,12 +95,13 @@ export default function DronePage() {
             }
         )
         if (image.length > 0) {
+            console.log(image);
             for (let index = 0; index < image.length; index++) {
                 let element = image[index];
                 console.log(element);
                 let data = new FormData()
                 data.append('image', element)
-                fetch('https://skydrone-api.herokuapp.com/api/v1/images', {
+                fetch('https://skydrone-api.herokuapp.com/api/v1/images/' + id, {
                     method: 'POST',
                     headers: {
                         'Authorization': 'Bearer ' + user.token},
@@ -98,15 +119,22 @@ export default function DronePage() {
         event.preventDefault();
     } 
 
-    const [title, setTitle] = useState("")
-    const [desc, setDesc] = useState("")
-    const [price, setPrice] = useState("")
-
-    useEffect (() => {
-        setTitle(drone.name_d)
-        setDesc(drone.description_d)
-        setPrice(drone.pricePerDay_d)
-    }, [drone])
+    const handleChange = (event, type) => {
+        const { name, value } = event.target
+        switch (type) {
+            case 'drone':
+                setData(prev => ({
+                    ...prev,
+                    drone: {
+                        ...prev.drone,
+                        [name]: value
+                    }
+                }))
+                break;
+            default:
+                break;
+        }
+    }
 
     const handleImagePreview = (files) => {
             const allFiles = files.target.files
@@ -116,7 +144,8 @@ export default function DronePage() {
                 Array.from(allFiles).forEach(file => {
                     const img = document.createElement('img')
                     img.classList.add('image-preview')
-                    img.src = URL.createObjectURL(file)
+                    let imgSrc = URL.createObjectURL(file)
+                    img.src = imgSrc
                     image.parentNode.insertBefore(img, image.nextSibling)
                     setImage(previousState => [...previousState, file])
                 })
@@ -127,30 +156,29 @@ export default function DronePage() {
     <>
     <h1>Produit</h1>
     <div className="row mt-3">
-        
-        <form className="col-8" onSubmit={handleSubmit} >
+        <form className="col-8" onSubmit={id ? handleSubmit : handleSubmitNew} >
             <h2>Informations</h2>
             <div className="card p-4" >
                 <div className="mb-3">
                     <label htmlFor="title" className="form-label">Titre</label>
-                    <input type="text" className="form-control" id="title" placeholder="Titre du produit" value={title} onChange={e => setTitle(e.target.value)}></input>
+                    <input type="text" name='name_d' className="form-control" id="title" placeholder="Titre du produit" value={data.drone.name_d || ''} onChange={ e => handleChange(e, 'drone')}></input>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="desc" className="form-label">Description</label>
-                    <textarea className="form-control" id="desc" rows="8" placeholder="Description du produit"  value={desc} onChange={e => setDesc(e.target.value)}></textarea>
+                    <textarea className="form-control" name='description_d' id="desc" rows="6" placeholder="Description du produit"  value={data.drone.description_d || ''} onChange={ e => handleChange(e, 'drone')}></textarea>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="price" className="form-label">Prix</label>
-                    <input type="number" className="form-control" id="price" placeholder="Prix du produit"  value={price} onChange={e => setPrice(e.target.value)}></input>
+                    <input type="number" name='pricePerDay_d' className="form-control" id="price" placeholder="Prix du produit"  value={data.drone.pricePerDay_d || ''} onChange={ e => handleChange(e, 'drone')}></input>
                 </div>
                 <div className="mb-0">
                     <label htmlFor="image" className="form-label">Images</label>
                     <input type="file" className="form-control" id="image" multiple onChange={e => handleImagePreview(e)}></input>
                     <div className='border container-images d-flex align-items-center mt-2'>
-                        {imgArray.map((img, index) => {
+                        {data.images.map((img, index) => {
                                 return (
                         <div className='card-images col-2' key={index}>
-                                {img}
+                                <img src={img}></img>
                         </div>
                             )})}  
                     </div>
